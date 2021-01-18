eff6850145ea021970799e3d2fc1879eb4d70bac
Umut Bayraktar <69085038+umutbayraktar0@users.noreply.github.com>
2021-01-18 09:22:45 -0800

    Create reaction.js

---
+++ reaction.js
@@ -0,0 +1,20 @@
+const reactions = [
+
+    {
+        sunucu: 'SUNUCU ID YAZ',
+        kanal: 'MESAJIN OLDUGU KANAL ID YAZ',
+        mesaj: 'MESAJ ID YAZ',
+        emojiid: 'EMOJI ID YAZ',
+        rol: 'VERILECEK ROL ID YAZ'
+    },
+      {
+        sunucu: 'SUNUCU ID YAZ',
+        kanal: 'MESAJIN OLDUGU KANAL ID YAZ',
+        mesaj: 'MESAJ ID YAZ',
+        emojiid: 'EMOJI ID YAZ',
+        rol: 'VERILECEK ROL ID YAZ'
+    }
+    //Bu Şekilde } Sonuna , Koyarak Çoğalta Bilirsiniz!
+];
+
+module.exports = reactions;
